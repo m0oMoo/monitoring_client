34fc246fc13b4af239916dfa5601f1931fc9bc8e
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-02-18 17:24:27 +0900

    feat: Update addTabModal.tsx

--- src/app/components/modal/addTabModal.tsx
+++ src/app/components/modal/addTabModal.tsx
@@ -3,24 +3,27 @@ import React, { useState } from "react";
 interface AddTabModalProps {
   isOpen: boolean;
   onClose: () => void;
-  onAddTab: (tabName: string) => void;
+  onAddTab: (tabName: string, tabDescription: string) => void;
 }
 
 const AddTabModal = ({ isOpen, onClose, onAddTab }: AddTabModalProps) => {
-  const [newTabName, setNewTabName] = useState("");
+  const [newTabName, setNewTabName] = useState<string>("");
+  const [newTabDescription, setNewTabDescription] = useState<string>("");
 
   const handleSubmit = () => {
     if (newTabName.trim()) {
-      onAddTab(newTabName);
+      onAddTab(newTabName, newTabDescription);
       setNewTabName("");
+      setNewTabDescription("");
     }
   };
 
   return (
     isOpen && (
-      <div className="fixed inset-0 bg-gray-0 bg-opacity-50 flex justify-center items-center z-50">
+      <div className="fixed inset-0 bg-black bg-opacity-20 backdrop-blur-sm flex justify-center items-center z-50">
         <div className="bg-[#fdf7e4] p-6 rounded-lg shadow-lg w-96 border border-[#1f3e5c]">
           <h2 className="text-xl text-[#1f3e5c] mb-4">새 탭 추가</h2>
+
           <input
             type="text"
             value={newTabName}
@@ -28,6 +31,14 @@ const AddTabModal = ({ isOpen, onClose, onAddTab }: AddTabModalProps) => {
             className="border border-[#1f3e5c] p-2 mb-4 w-full bg-[#F5F5F5] text-[#102841] placeholder-[#B0C4DE] rounded-lg"
             placeholder="탭 이름을 입력하세요"
           />
+
+          <textarea
+            value={newTabDescription}
+            onChange={(e) => setNewTabDescription(e.target.value)}
+            className="border border-[#1f3e5c] p-2 mb-4 w-full bg-[#F5F5F5] text-[#102841] placeholder-[#B0C4DE] rounded-lg"
+            placeholder="탭 설명을 입력하세요"
+          />
+
           <div className="flex justify-end gap-4">
             <button
               className="px-4 py-2 bg-[#1f3e5c] text-white rounded-lg hover:bg-[#102841]"
