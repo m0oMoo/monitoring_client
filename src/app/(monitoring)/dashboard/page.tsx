97bc20e067013225061b2292f12d313367e368a2
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-02-21 17:20:39 +0900

    design: Update page.tsx

--- src/app/(monitoring)/dashboard/page.tsx
+++ src/app/(monitoring)/dashboard/page.tsx
@@ -5,20 +5,27 @@ import AddTabModal from "@/app/components/modal/addTabModal";
 import TabsGroup from "@/app/components/tab/tabsGroup";
 import { Tabs } from "@/app/components/tab/tabs";
 import { DEFAULT_DASHBOARD_DATA } from "@/app/data/dashboardData";
-import {
-  ArrowBigDown,
-  ArrowDown,
-  ArrowDownUp,
-  ChevronDown,
-  ChevronRight,
-} from "lucide-react";
+import SearchInput from "@/app/components/search/searchInput";
 
 const Dashboard = () => {
   const [tabs, setTabs] = useState<any[]>(DEFAULT_DASHBOARD_DATA);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [selectedTabIndex, setSelectedTabIndex] = useState<number>(0);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [editingTabIndex, setEditingTabIndex] = useState<number | null>(null);
 
-  const handleAddTab = (newTabName: string, newTabDescription: string) => {
+  // 검색 처리 함수
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  // 필터링된 탭 목록
+  const filteredTabs = tabs.filter((tab) =>
+    tab.label.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  // 탭 추가
+  const handleTabAdd = (newTabName: string, newTabDescription: string) => {
     const newTabValue = `${tabs.length + 1}`;
     setTabs([
       ...tabs,
@@ -32,44 +39,53 @@ const Dashboard = () => {
     setIsModalOpen(false);
   };
 
+  // 탭 수정
   const handleTabEdit = (index: number, newName: string) => {
     const updatedTabs = [...tabs];
     updatedTabs[index].label = newName;
     setTabs(updatedTabs);
   };
 
+  // 탭 선택
   const handleTabSelect = (index: number) => {
     setSelectedTabIndex(index);
   };
 
+  // 선택된 탭
   const selectedTab = tabs[selectedTabIndex];
 
   return (
-    <div className="bg-ivory-bg text-navy-text min-h-screen p-4 pt-[44px]">
+    <div className="bg-ivory-bg_sub text-navy-text min-h-screen p-4 pt-[44px]">
       <header className="flex justify-between items-center my-3">
         <h1 className="text-xl font-bold tracking-wide">
           📊 모니터링 대시보드
         </h1>
-        <button
-          onClick={() => setIsModalOpen(true)}
-          className="bg-navy-btn py-1.5 px-2 rounded-lg text-white text-sm hover:bg-navy-btn_hover"
-        >
-          + 탭 추가
-        </button>
+        <SearchInput
+          searchQuery={searchQuery}
+          onSearchChange={handleSearchChange}
+        />
       </header>
 
+      <button
+        onClick={() => setIsModalOpen(true)}
+        className="flex bg-navy-btn py-1.5 px-2 rounded-lg text-white text-sm hover:bg-navy-btn_hover mb-4 justify-self-end"
+      >
+        + 항목 추가
+      </button>
+
+      {/* Tabs 컴포넌트 */}
       <Tabs
         defaultValue="dashboard1"
         className="w-full flex justify-between pb-2"
       >
         <TabsGroup
-          tabs={tabs}
+          tabs={filteredTabs}
           onTabEdit={handleTabEdit}
           onTabSelect={handleTabSelect}
         />
       </Tabs>
-      <div className="w-full border-b border-0.5 border-navy-border" />
-      {/* <ChevronDown /> */}
+
+      <div className="w-full mb-2 border-b border-0.5 border-navy-border" />
 
       {/* 대시보드 내용 */}
       <div className="mt-6 p-4 bg-white rounded-lg shadow-md">
@@ -81,12 +97,19 @@ const Dashboard = () => {
         </h3>
         <p className="text-navy-text mt-2">{selectedTab.content}</p>
       </div>
-      <>여기에 컨텐츠가 들어가야함.</>
 
       <AddTabModal
         isOpen={isModalOpen}
         onClose={() => setIsModalOpen(false)}
-        onAddTab={handleAddTab}
+        onAddTab={handleTabAdd}
+        initialTabName={
+          editingTabIndex !== null ? tabs[editingTabIndex].label : ""
+        }
+        initialTabDescription={
+          editingTabIndex !== null ? tabs[editingTabIndex].description : ""
+        }
+        onEditTab={handleTabEdit}
+        editingIndex={editingTabIndex}
       />
     </div>
   );
