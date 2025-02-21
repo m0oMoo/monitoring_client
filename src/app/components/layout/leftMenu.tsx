023fd6d8b60c8c9f71e59d77aba60bcee1ad9496
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-02-21 17:18:39 +0900

    feat: Update leftMenu.tsx

--- src/app/components/layout/leftMenu.tsx
+++ src/app/components/layout/leftMenu.tsx
@@ -1,29 +1,58 @@
-import { LayoutGrid, FileText } from "lucide-react";
+"use client";
+
+import { LayoutGrid, FileText, Database } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
+const MENU = [
+  { id: 1, title: "Dashboard", icon: LayoutGrid, link: "/dashboard2" },
+  { id: 2, title: "Reports", icon: FileText, link: "/reports" },
+  {
+    id: 3,
+    title: "Data Source",
+    icon: Database,
+    link: "/connection/dataSource",
+  },
+  {
+    id: 4,
+    title: "Create Dashboard",
+    icon: LayoutGrid,
+    link: "/d",
+  },
+];
 
 const LeftMenu = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleMenuClick = (link: string) => {
+    router.push(link);
+  };
+
   return (
     <nav
-      className="w-60 bg-[#fdfdf4] p-4 shadow-xl h-full flex flex-col space-y-4
+      className="w-60 bg-ivory-bg_sub p-4 shadow-xl h-full flex flex-col space-y-4
     border-r border-0.5 border-navy-border"
     >
       <div className="flex items-center space-x-2">
-        <span className="text-lg font-semibold text-navy-text">Menu</span>
+        <span className="mb-6 text-title_sm text-navy-text">Menu</span>
       </div>
-      <div className="flex flex-col space-y-3 mt-4 text-base">
-        <div
-          className="flex items-center space-x-2 cursor-pointer 
-          text-navy-text_disable hover:text-navy-text"
-        >
-          <LayoutGrid className="w-4 h-4 " />
-          <span>Dashboard</span>
-        </div>
-        <div
-          className="flex items-center space-x-2 cursor-pointer 
-        text-navy-text_disable hover:text-navy-text"
-        >
-          <FileText className="w-4 h-4 " />
-          <span>Reports</span>
-        </div>
+      <div className="flex flex-col space-y-4 text-base">
+        {MENU.map((item) => (
+          <div
+            key={item.id}
+            className={`flex items-center space-x-2 cursor-pointer pl-4 
+              ${
+                pathname === item.link
+                  ? "text-primary border-l-2 border-navy-border text-bold_lg"
+                  : "text-lg1 text-navy-text_disable"
+              } 
+              hover:text-navy-text`}
+            onClick={() => handleMenuClick(item.link)}
+          >
+            <item.icon className="w-4 h-4" />
+            <span>{item.title}</span>
+          </div>
+        ))}
       </div>
     </nav>
   );
