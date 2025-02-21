4053af9b737e107f9a57f903b9a6351b32df5ff8
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-02-21 17:19:38 +0900

    feat: Create connection page

---
+++ src/app/(connection)/connection/dataSource/page.tsx
@@ -0,0 +1,11 @@
+import DataSourceList from "./content/dataSourceList";
+
+const DataSourcePage = () => {
+  return (
+    <div className="bg-ivory-bg_sub">
+      <DataSourceList />
+    </div>
+  );
+};
+
+export default DataSourcePage;
