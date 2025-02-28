3a304d94a887bb7bdb2d3a4447a1d66aad5d325d
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-02-28 16:41:10 +0900

    feat: Add store

--- src/app/store/useChartStore.ts
+++ src/app/store/useChartStore.ts
@@ -1,14 +1,17 @@
 import { create } from "zustand";
+import { v4 as uuidv4 } from "uuid";
 
 interface ChartStore {
-  charts: Record<string, any[]>;
+  charts: Record<string, any[]>; // 대시보드별 차트들
   setChartData: (
     dashboardId: string,
     chartData: any,
     chartOptions: any,
     chartType: string,
-    titleText: string
-  ) => void;
+    titleText: string,
+    chartId?: string
+  ) => void; // chartId가 있을 때 기존 차트 업데이트
+  removeChart: (dashboardId: string, chartId: string) => void;
 }
 
 export const useChartStore = create<ChartStore>((set) => ({
@@ -18,16 +21,56 @@ export const useChartStore = create<ChartStore>((set) => ({
     chartData,
     chartOptions,
     chartType,
-    titleText
+    titleText,
+    chartId
   ) => {
-    set((state) => ({
-      charts: {
-        ...state.charts,
-        [dashboardId]: [
-          ...(state.charts[dashboardId] || []),
-          { chartData, chartOptions, chartType, titleText },
-        ],
-      },
-    }));
+    if (chartId) {
+      // 차트가 존재하는 경우 업데이트
+      set((state) => {
+        const updatedCharts = state.charts[dashboardId].map((chart) =>
+          chart.chartId === chartId
+            ? { ...chart, chartData, chartOptions, chartType, titleText } // 기존 차트 업데이트
+            : chart
+        );
+        return {
+          charts: {
+            ...state.charts,
+            [dashboardId]: updatedCharts,
+          },
+        };
+      });
+    } else {
+      // 차트가 존재하지 않으면 새로 추가
+      const newChartId = uuidv4(); // 새 chartId 생성
+      set((state) => ({
+        charts: {
+          ...state.charts,
+          [dashboardId]: [
+            ...(state.charts[dashboardId] || []),
+            {
+              chartId: newChartId,
+              chartData,
+              chartOptions,
+              chartType,
+              titleText,
+            },
+          ],
+        },
+      }));
+    }
+  },
+  removeChart: (dashboardId: string, chartId: string) => {
+    set((state) => {
+      const newCharts =
+        state.charts[dashboardId]?.filter(
+          (chart) => chart.chartId !== chartId
+        ) || [];
+      return {
+        charts: {
+          ...state.charts,
+          [dashboardId]: newCharts,
+        },
+      };
+    });
   },
 }));
