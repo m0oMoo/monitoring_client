80ceb379777f5cc4ef4284ea52d538791b805c07
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-03-04 13:06:31 +0900

    feat: Add gridCols button

--- src/app/components/bar/addChartBar.tsx
+++ src/app/components/bar/addChartBar.tsx
@@ -1,21 +1,48 @@
-import { useRouter } from "next/navigation";
+import { MinusCircle, PlusCircle } from "lucide-react";
 
 interface AddChartBarProps {
   isEdit: boolean;
   onCreateClick: () => void;
   onSaveClick?: () => void;
+  gridCols?: number;
+  onGridChange?: (change: number) => void;
+  gridVisible?: boolean;
 }
 
 const AddChartBar = ({
   isEdit,
   onCreateClick,
   onSaveClick,
+  gridCols,
+  onGridChange,
+  gridVisible = false,
 }: AddChartBarProps) => {
   return (
     <div
-      className="flex justify-end items-center mt-[44px] bg-transparent
-      border-b border-navy-border border-0.5 py-1.5"
+      className={`flex ${
+        gridVisible ? "justify-between" : "justify-end"
+      }  items-center mt-[44px] bg-transparent
+      border-b border-navy-border border-0.5 py-1.5`}
     >
+      {gridVisible && (
+        <div className={`flex justify-end items-center space-x-2 pl-3`}>
+          <button
+            onClick={() => onGridChange && onGridChange(-1)}
+            disabled={gridCols === 1}
+            className="p-1 rounded-md text-white bg-navy-btn hover:bg-navy-btn_hover disabled:bg-gray-300 disabled:cursor-not-allowed"
+          >
+            <MinusCircle size={16} />
+          </button>
+          <span className="text-sm_bold">{gridCols} Columns</span>
+          <button
+            onClick={() => onGridChange && onGridChange(1)}
+            disabled={gridCols === 4}
+            className="p-1 rounded-md text-white bg-navy-btn hover:bg-navy-btn_hover disabled:bg-gray-300 disabled:cursor-not-allowed"
+          >
+            <PlusCircle size={16} />
+          </button>
+        </div>
+      )}
       <div className="flex flex-row gap-2 mr-4">
         <button
           className={`text-text text-sm_bold border-2 border-navy-border
