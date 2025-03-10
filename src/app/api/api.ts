affca23858172471d1bcca9f75cbc0a4a64dd29f
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-03-10 14:36:30 +0900

    feat: Delete src/app/api directory

--- src/app/api/api.ts
+++
@@ -1,9 +0,0 @@
-import { generateFakeDashboard } from "../data/faker";
-
-export const fetchDashboardData = async () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(generateFakeDashboard());
-    }, 1000); // 1초 후 데이터 반환 (API 지연 효과)
-  });
-};
