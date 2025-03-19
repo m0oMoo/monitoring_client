ac188ab727b09fcbf583217d667380dedbda333c
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-03-19 15:52:25 +0900

    feat: Add chart data

---
+++ src/app/data/chart/chartDetail.ts
@@ -0,0 +1,9 @@
+export const MIN_CHART_WIDTH = 6; // 차트 최소 가로 크기
+export const MIN_CHART_HEIGHT = 6; // 차트 최소 세로 크기
+export const MAX_CHART_HEIGHT = 10; // 차트 최대 세로 크기
+export const MAX_CHART_WIDTH = 4; // 차트 최대 세로 크기
+
+export const MIN_WIDGET_WIDTH = 6; // 위젯 최소 가로 크기
+export const MIN_WIDGET_HEIGHT = 2; // 위젯 최소 세로 크기
+export const MAX_WIDGET_HEIGHT = 6; // 위젯 최대 세로 크기
+export const MAX_WIDGET_WIDTH = 6; // 위젯 최대 가로 크기
