9dcb23099372344ec49659cee86fa446f7331241
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-03-19 15:53:43 +0900

    feat: Update detailDashboard.tsx

--- src/app/(monitoring)/detail/content/detailDashboard.tsx
+++ src/app/(monitoring)/detail/content/detailDashboard.tsx
@@ -5,8 +5,6 @@ import { Chart, useChartStore } from "@/app/store/useChartStore";
 import { PanelLayout, useDashboardStore } from "@/app/store/useDashboardStore";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Layout, Responsive, WidthProvider } from "react-grid-layout";
-import "react-grid-layout/css/styles.css";
-import "react-resizable/css/styles.css";
 import AddChartBar from "@/app/components/bar/addChartBar";
 import TimeRangeBar from "@/app/components/bar/timeRangeBar";
 import ChartWidget from "@/app/components/dashboard/chartWidget";
@@ -15,9 +13,19 @@ import CustomTable from "@/app/components/table/customTable";
 import TabMenu from "@/app/components/menu/tabMenu";
 import { MoreVertical } from "lucide-react";
 import { useWidgetStore, Widget } from "@/app/store/useWidgetStore";
-import { Dataset } from "@/app/context/chartOptionContext";
 import { v4 as uuidv4 } from "uuid";
 import Alert from "@/app/components/alert/alert";
+import { convertToTable } from "@/app/utils/convertToTable";
+import {
+  MIN_CHART_WIDTH,
+  MIN_CHART_HEIGHT,
+  MIN_WIDGET_WIDTH,
+  MIN_WIDGET_HEIGHT,
+  MAX_WIDGET_WIDTH,
+  MAX_WIDGET_HEIGHT,
+  MAX_CHART_HEIGHT,
+  MAX_CHART_WIDTH,
+} from "@/app/data/chart/chartDetail";
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
@@ -31,29 +39,6 @@ const DetailDashboard = () => {
   const { dashboardPanels, addPanelToDashboard, dashboardList, saveDashboard } =
     useDashboardStore();
 
-  const [, forceUpdate] = useState(0);
-
-  useEffect(() => {
-    console.log(
-      "🚀 Zustand 상태 확인 (차트):",
-      useChartStore.getState().charts
-    );
-    console.log(
-      "🚀 Zustand 상태 확인 (위젯):",
-      useWidgetStore.getState().widgets
-    );
-
-    console.log(
-      "🚀 Zustand 상태 확인 (대시보드):",
-      useDashboardStore.getState().dashboardPanels
-    );
-
-    // 🚀 강제로 상태 업데이트
-    forceUpdate((prev) => prev + 1);
-  }, []);
-
-  const chartIds =
-    dashboardPanels[dashboardId]?.map((panel) => panel.panelId) || [];
   const [from, setFrom] = useState<string | null>(null);
   const [to, setTo] = useState<string | null>(null);
   const [refreshTime, setRefreshTime] = useState<number | "autoType">(10);
@@ -68,8 +53,6 @@ const DetailDashboard = () => {
   const [gridLayout, setGridLayout] = useState<
     { i: string; x: number; y: number; w: number; h: number }[]
   >([]);
-  const [maxWidth, setMaxWidth] = useState<number>(500);
-  const [maxHeight, setMaxHeight] = useState<number>(500);
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const [prevLayout, setPrevLayout] = useState<Layout[]>([]);
 
@@ -94,28 +77,6 @@ const DetailDashboard = () => {
     }
     setIsEditing((prev) => !prev);
   };
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      // 브라우저에서만 실행
-      setMaxWidth(window.innerWidth);
-      setMaxHeight(window.innerHeight - 100);
-
-      const handleResize = () => {
-        setMaxWidth(window.innerWidth);
-        setMaxHeight(window.innerHeight - 100);
-      };
-
-      window.addEventListener("resize", handleResize);
-      return () => window.removeEventListener("resize", handleResize);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      localStorage.setItem("dashboard-layout", JSON.stringify(gridLayout));
-    }
-  }, [gridLayout]);
 
   const chartDataList = (dashboardPanels[dashboardId] || [])
     .filter((panel) => panel.type === "chart")
@@ -186,97 +147,20 @@ const DetailDashboard = () => {
     setIsCloneModalOpen(false);
   };
 
-  const convertToTableData = (datasets: Dataset[]) => {
-    if (!datasets || datasets.length === 0) return { headers: [], rows: [] };
-
-    const headers = ["항목", ...datasets.map((dataset) => dataset.label)];
-
-    const rows = datasets[0].data.map((_, index) => ({
-      name: `Point ${index + 1}`,
-      ...datasets.reduce((acc, dataset) => {
-        acc[dataset.label] = dataset.data[index];
-        return acc;
-      }, {} as Record<string, any>),
-    }));
-
-    return { headers, rows };
-  };
-
-  const MIN_CHART_WIDTH = 6; // 차트 최소 가로 크기
-  const MIN_CHART_HEIGHT = 5; // 차트 최소 세로 크기
-  const MAX_CHART_HEIGHT = 10; // 차트 최소 세로 크기
-  const MIN_WIDGET_WIDTH = 3; // 위젯 최소 가로 크기
-  const MIN_WIDGET_HEIGHT = 4; // 위젯 최소 세로 크기
-  const MAX_WIDGET_HEIGHT = 6; // 위젯 최소 세로 크기
-
-  const initialLayout = useMemo(() => {
-    return [
-      ...chartDataList.map((chart, index) => ({
-        i: chart.chartId,
-        x: (index * 6) % 12, // 한 줄에 2개 배치
-        y: Math.floor(index / 2) * 5, // 차트 배치 간격 조정
-        w: Math.max(MIN_CHART_WIDTH, maxWidth / 200), // 최소 크기 보장
-        h: Math.max(
-          MIN_CHART_HEIGHT,
-          chart.chartOptions.displayMode === "chart" ? 5 : 6
-        ),
-      })),
-      ...widgetDataList.map((widget, index) => ({
-        i: widget.widgetId,
-        x: (index * 3) % 12, // 한 줄에 최대 4개 배치
-        y: Math.floor(index / 4) * 4 + chartDataList.length * 5,
-        w: Math.max(MIN_WIDGET_WIDTH, maxWidth / 250), // 최소 크기 보장
-        h: MIN_WIDGET_HEIGHT,
-      })),
-    ];
-  }, [chartDataList, widgetDataList, maxWidth]);
-
-  useEffect(() => {
-    const savedLayout = localStorage.getItem("dashboard-layout");
-    if (savedLayout) {
-      // ✅ 기존에 저장된 레이아웃이 있으면 그걸 적용
-      console.log(
-        "📌 LocalStorage에서 불러온 레이아웃:",
-        JSON.parse(savedLayout)
-      );
-      setGridLayout(JSON.parse(savedLayout));
-    } else if (gridLayout.length === 0 && initialLayout.length > 0) {
-      // ✅ 기존에 저장된 값이 없고, 초기 레이아웃이 있을 때만 설정
-      console.log(
-        "📌 저장된 레이아웃 없음, 초기 레이아웃 적용:",
-        initialLayout
-      );
-      setGridLayout(initialLayout);
-    }
-  }, []);
-
   const closeCloneModal = () => {
     setIsCloneModalOpen(false);
     setSelectedDashboard(null);
   };
 
-  useEffect(() => {
-    const savedLayout = localStorage.getItem("dashboard-layout");
-    if (savedLayout) {
-      setGridLayout(JSON.parse(savedLayout)); // 저장된 위치 적용
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("dashboard-layout", JSON.stringify(gridLayout));
-  }, [gridLayout]);
   const handleLayoutChange = (layout: Layout[]) => {
-    // 변경 사항이 없으면 상태 업데이트 하지 않음
+    // 상태가 변경되지 않았다면 업데이트하지 않음
     if (JSON.stringify(prevLayout) === JSON.stringify(layout)) {
       return;
     }
 
-    console.log("📌 변경된 레이아웃:", layout);
-    setGridLayout(layout);
-    setPrevLayout(layout); // 이전 상태 업데이트
-    // ✅ `i`를 `panelId`로 변환하여 저장
+    // 레이아웃이 변경된 경우에만 처리
     const updatedLayouts: PanelLayout[] = layout.map((l) => ({
-      panelId: l.i, // ✅ `i`를 `panelId`로 매핑
+      panelId: l.i, // `i`를 `panelId`로 매핑
       type:
         dashboardPanels[dashboardId]?.find((p) => p.panelId === l.i)?.type ||
         "chart",
@@ -286,13 +170,10 @@ const DetailDashboard = () => {
       h: l.h,
     }));
 
-    console.log("📌 변경된 레이아웃:", updatedLayouts);
-
-    if (JSON.stringify(prevLayout) !== JSON.stringify(updatedLayouts)) {
-      setGridLayout(layout); // ✅ gridLayout에는 `i`를 유지
-      setPrevLayout(layout);
-      saveDashboard(dashboardId, updatedLayouts); // ✅ Zustand에는 `panelId`로 저장
-    }
+    // 레이아웃 변경이 실제로 있을 때만 상태 업데이트
+    setGridLayout(layout);
+    setPrevLayout(layout); // 이전 레이아웃 갱신
+    saveDashboard(dashboardId, updatedLayouts); // Zustand에 레이아웃 저장
   };
 
   useEffect(() => {
@@ -338,17 +219,15 @@ const DetailDashboard = () => {
         }
         onRefreshChange={setRefreshTime}
       />
-      <div className="relative w-full" style={{ maxHeight }}>
+      <div className="relative w-full">
         <ResponsiveGridLayout
           className="layout"
-          layouts={layouts} // `layouts` 객체로 전달
+          layouts={layouts}
           rowHeight={70}
           isDraggable={isEditing}
           isResizable={isEditing}
           compactType={null}
-          // compactType="vertical" // 자동으로 세로 정렬 유지
-          preventCollision={true} // 패널 간 충돌 방지 여부
-          // preventCollision={false}
+          preventCollision={true}
           onLayoutChange={handleLayoutChange}
           maxRows={20} // 최대 줄 수 제한
           draggableHandle=".drag-handle"
@@ -360,20 +239,22 @@ const DetailDashboard = () => {
             ) || {
               i: chart.chartId,
               x: 0,
-              y: MAX_CHART_HEIGHT,
-              w: Math.min(4, maxWidth / 250),
-              h: 4,
+              y: 0,
+              w: 4,
+              h: Math.max(MIN_WIDGET_HEIGHT, 4),
+              minW: MIN_CHART_WIDTH,
+              minH: MIN_CHART_HEIGHT,
             };
 
             return (
               <div
                 key={chart.chartId}
-                data-grid={chartLayout}
-                className={`drag-handle cursor-grab ${
-                  chart.chartOptions.displayMode === "table"
-                    ? "max-h-[500px]"
-                    : ""
-                }`}
+                data-grid={{
+                  ...chartLayout,
+                  minW: MIN_CHART_WIDTH,
+                  minH: MIN_CHART_HEIGHT,
+                }}
+                className={`drag-handle cursor-grab`}
               >
                 <div className="border rounded-lg bg-white p-4 shadow-md h-full flex flex-col relative">
                   {/* 메뉴 버튼 (기존 유지) */}
@@ -427,7 +308,7 @@ const DetailDashboard = () => {
                             label: dataset.label,
                           })),
                         ]}
-                        data={convertToTableData(chart.datasets).rows}
+                        data={convertToTable(chart.datasets).rows}
                         title={chart.chartOptions.titleText}
                       />
                     )}
@@ -443,19 +324,26 @@ const DetailDashboard = () => {
             ) || {
               i: widget.widgetId,
               x: 0,
-              y: MAX_WIDGET_HEIGHT,
-              w: Math.min(2, maxWidth / 250),
-              h: 3,
+              y: 0,
+              w: 4, // 기본 가로 크기
+              h: Math.max(MIN_WIDGET_HEIGHT, 4), // 기본 세로 크기
+              minW: MIN_WIDGET_WIDTH, // 최소 가로 크기 설정
+              minH: MIN_WIDGET_HEIGHT, // 최소 세로 크기 설정
             };
 
             return (
               <div
                 key={widget.widgetId}
-                data-grid={widgetLayout}
-                // style={{ maxWidth }}
-                className="drag-handle cursor-grab max-h-[230px] max-w-[530px]"
+                data-grid={{
+                  ...widgetLayout, // widgetLayout에서 x, y, w, h 값을 그대로 사용
+                  minW: 2,
+                  maxW: 4,
+                  minH: MIN_WIDGET_HEIGHT, // 최소 세로 크기 설정
+                }}
+                className="drag-handle cursor-grab"
               >
-                <div className="relative flex flex-col h-full">
+                {/* max-h-[230px] max-w-[530px] */}
+                <div className="relative flex flex-col h-full min-w-72 min-h-32">
                   <div className="absolute top-2 right-2 z-10 pointer-events-auto">
                     <MoreVertical
                       className="text-text3 cursor-pointer hover:text-text2"
